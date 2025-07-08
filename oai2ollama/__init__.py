608b2392c54151c37e218c5057e2c68befd7fc20
Liny <reddisk@foxmail.com>
2025-07-08 21:04:18 +0800

    Uvicorn to listen on ipv4

--- oai2ollama/__init__.py
+++ oai2ollama/__init__.py
@@ -3,8 +3,8 @@ from ._app import app
 
 def start():
     import uvicorn
-
-    uvicorn.run(app, host="::", port=11434)
+    # 监听 IPv4 0.0.0.0，确保主机端口可访问
+    uvicorn.run(app, host="0.0.0.0", port=11434)
 
 
 __all__ = ["_app", "start"]
